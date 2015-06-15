192c2bf59ee656c88dbb157dc7f6e720b73e150f
ugiacoman <ulises.giacoman@gmail.com>
2015-06-15 14:01:43 -0400

    initial upload

---
+++ demo/app/scripts/app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularGanttDemoApp
+ * @description
+ * # angularGanttDemoApp
+ *
+ * Main module of the application.
+ */
+angular.module('angularGanttDemoApp', [
+    'gantt', // angular-gantt.
+    'gantt.sortable',
+    'gantt.movable',
+    'gantt.drawtask',
+    'gantt.tooltips',
+    'gantt.bounds',
+    'gantt.progress',
+    'gantt.table',
+    'gantt.tree',
+    'gantt.groups',
+    'gantt.overlap',
+    'gantt.resizeSensor',
+    'mgcrea.ngStrap'
+]).config(['$compileProvider', function($compileProvider) {
+    $compileProvider.debugInfoEnabled(true); // Remove debug info (angularJS >= 1.3)
+}]);
