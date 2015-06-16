79f839dec4c33a448581ab19ad7c6dd2eaac09ed
jds5vf <sacks@logapps.com>
2015-06-16 14:10:16 -0400

    Cost row included now

--- demo/dist/scripts/scripts.js
+++ demo/dist/scripts/scripts.js
@@ -49,23 +49,29 @@ angular.module('angularGanttDemoApp')
             maxHeight: false,
             width: false,
             zoom: 1,
-            columns: ['model.name', 'from', 'to'],
-            treeTableColumns: ['from', 'to'],
-            columnsHeaders: {'model.name' : 'Name', 'from': 'From', 'to': 'To'},
-            columnsClasses: {'model.name' : 'gantt-column-name', 'from': 'gantt-column-from', 'to': 'gantt-column-to'},
+            columns: ['model.name', 'from', 'to', 'cost'],
+            treeTableColumns: ['from', 'to', 'cost'],
+            columnsHeaders: {'model.name' : 'Name', 'from': 'From', 'to': 'To', 'cost': 'Cost'},
+            columnsClasses: {'model.name' : 'gantt-column-name', 'from': 'gantt-column-from', 'to': 'gantt-column-to', 'cost': 'gannt-column-cost'},
             columnsFormatters: {
                 'from': function(from) {
                     return from !== undefined ? from.format('lll') : undefined;
                 },
                 'to': function(to) {
                     return to !== undefined ? to.format('lll') : undefined;
-                }
+                },
+				'cost': function(cost) {
+					return to !== undefined ? cost.format('111') : undefined;
+				},
+               
             },
+			//Put in icons next to header titles
             treeHeaderContent: '<i class="fa fa-align-justify"></i> {{getHeader()}}',
             columnsHeaderContents: {
                 'model.name': '<i class="fa fa-align-justify"></i> {{getHeader()}}',
                 'from': '<i class="fa fa-calendar"></i> {{getHeader()}}',
-                'to': '<i class="fa fa-calendar"></i> {{getHeader()}}'
+                'to': '<i class="fa fa-calendar"></i> {{getHeader()}}',
+				'cost': '<i class="fa fa-usd"></i> {{getHeader()}}'
             },
             autoExpand: 'none',
             taskOutOfRange: 'truncate',
@@ -479,22 +485,46 @@ angular.module('angularGanttDemoApp')
             getSampleData: function() {
                 return [
                         // Order is optional. If not specified it will be assigned automatically
-                        {name: 'Milestones', height: '3em', sortable: false, classes: 'gantt-row-milestone', color: '#45607D', tasks: [
+                        {name: 'All Systems', height: '3em', sortable: false, classes: 'gantt-row-milestone', color: '#45607D', tasks: [
                             // Dates can be specified as string, timestamp or javascript date object. The data attribute can be used to attach a custom object
-                            {name: 'Kickoff', color: '#93C47D', from: '2013-10-07T09:00:00', to: '2013-10-07T10:00:00', data: 'Can contain any custom data or object'},
-                            {name: 'Concept approval', color: '#93C47D', from: new Date(2013, 9, 18, 18, 0, 0), to: new Date(2013, 9, 18, 18, 0, 0), est: new Date(2013, 9, 16, 7, 0, 0), lct: new Date(2013, 9, 19, 0, 0, 0)},
-                            {name: 'Development finished', color: '#93C47D', from: new Date(2013, 10, 15, 18, 0, 0), to: new Date(2013, 10, 15, 18, 0, 0)},
-                            {name: 'Shop is running', color: '#93C47D', from: new Date(2013, 10, 22, 12, 0, 0), to: new Date(2013, 10, 22, 12, 0, 0)},
-                            {name: 'Go-live', color: '#93C47D', from: new Date(2013, 10, 29, 16, 0, 0), to: new Date(2013, 10, 29, 16, 0, 0)}
-                        ], data: 'Can contain any custom data or object'},
-                        {name: 'Status meetings', tasks: [
+                            {name: 'JIFMS', color: '#93C47D', from: '2013-10-07T09:00:00', to: '2013-10-07T10:00:00',},
+                            {name: 'CCAM', color: '#93C47D', from: new Date(2013, 9, 18, 18, 0, 0), to: new Date(2013, 9, 18, 18, 0, 0), est: new Date(2013, 9, 16, 7, 0, 0), lct: new Date(2013, 9, 19, 0, 0, 0)},
+                            {name: 'Ulises Project', color: '#93C47D', from: new Date(2013, 9, 18, 18, 0, 0), to: new Date(2013, 9, 18, 18, 0, 0), est: new Date(2013, 9, 16, 7, 0, 0), lct: new Date(2013, 9, 19, 0, 0, 0)},
+                            {name: 'Jordan Is Awesome', color: '#93C47D', from: new Date(2013, 9, 18, 18, 0, 0), to: new Date(2013, 9, 18, 18, 0, 0), est: new Date(2013, 9, 16, 7, 0, 0), lct: new Date(2013, 9, 19, 0, 0, 0)},
+
+                        ], 
+						//data: 'TEST'},
+						// {name: 'JIFMS', height: '3em', sortable: false, classes: 'gantt-row-milestone', color: '#45607D', tasks: [
+                            //Dates can be specified as string, timestamp or javascript date object. The data attribute can be used to attach a custom object
+                            // {name: 'JIFMS', color: '#93C47D', from: '2013-10-07T09:00:00', to: '2013-10-07T10:00:00', data: 'Can contain any custom data or object'},
+                            // {name: 'CCAM', color: '#93C47D', from: new Date(2013, 9, 18, 18, 0, 0), to: new Date(2013, 9, 18, 18, 0, 0), est: new Date(2013, 9, 16, 7, 0, 0), lct: new Date(2013, 9, 19, 0, 0, 0)},
+                           
+                        // ], 
+						
+						// data: 'TEST'},
+						// {name: 'Hello', height: '3em', sortable: false, classes: 'gantt-row-milestone', color: '#45607D', tasks: [
+                          // Dates can be specified as string, timestamp or javascript date object. The data attribute can be used to attach a custom object
+                            // {name: 'Build', color: '#93C47D', from: '2013-10-07T09:00:00', to: '2013-10-07T10:00:00', data: '$500'},
+                            // {name: 'Produce', color: '#93C47D', from: new Date(2013, 9, 18, 18, 0, 0), to: new Date(2013, 9, 18, 18, 0, 0), est: new Date(2013, 9, 16, 7, 0, 0), lct: new Date(2013, 9, 19, 0, 0, 0)},
+                           
+                        // ], 
+						
+						data: 'Can contain any custom data or object'},
+                        {name: 'CCAM', tasks: [
                             {name: 'Demo #1', color: '#9FC5F8', from: new Date(2013, 9, 25, 15, 0, 0), to: new Date(2013, 9, 25, 18, 30, 0)},
                             {name: 'Demo #2', color: '#9FC5F8', from: new Date(2013, 10, 1, 15, 0, 0), to: new Date(2013, 10, 1, 18, 0, 0)},
                             {name: 'Demo #3', color: '#9FC5F8', from: new Date(2013, 10, 8, 15, 0, 0), to: new Date(2013, 10, 8, 18, 0, 0)},
                             {name: 'Demo #4', color: '#9FC5F8', from: new Date(2013, 10, 15, 15, 0, 0), to: new Date(2013, 10, 15, 18, 0, 0)},
                             {name: 'Demo #5', color: '#9FC5F8', from: new Date(2013, 10, 24, 9, 0, 0), to: new Date(2013, 10, 24, 10, 0, 0)}
                         ]},
-                        {name: 'Kickoff', movable: {allowResizing: false}, tasks: [
+						{name: 'Ulises Project', tasks: [
+                            {name: 'Demo #1', color: '#9FC5F8', from: new Date(2013, 9, 25, 15, 0, 0), to: new Date(2013, 9, 25, 18, 30, 0)},
+                            {name: 'Demo #2', color: '#9FC5F8', from: new Date(2013, 10, 1, 15, 0, 0), to: new Date(2013, 10, 1, 18, 0, 0)},
+                            {name: 'Demo #3', color: '#9FC5F8', from: new Date(2013, 10, 8, 15, 0, 0), to: new Date(2013, 10, 8, 18, 0, 0)},
+                            {name: 'Demo #4', color: '#9FC5F8', from: new Date(2013, 10, 15, 15, 0, 0), to: new Date(2013, 10, 15, 18, 0, 0)},
+                            {name: 'Demo #5', color: '#9FC5F8', from: new Date(2013, 10, 24, 9, 0, 0), to: new Date(2013, 10, 24, 10, 0, 0)}
+                        ]},
+                        {name: 'JIFMS', movable: {allowResizing: false}, tasks: [
                             {name: 'Day 1', color: '#9FC5F8', from: new Date(2013, 9, 7, 9, 0, 0), to: new Date(2013, 9, 7, 17, 0, 0),
                                 progress: {percent: 100, color: '#3C8CF8'}, movable: false},
                             {name: 'Day 2', color: '#9FC5F8', from: new Date(2013, 9, 8, 9, 0, 0), to: new Date(2013, 9, 8, 17, 0, 0),
@@ -502,49 +532,12 @@ angular.module('angularGanttDemoApp')
                             {name: 'Day 3', color: '#9FC5F8', from: new Date(2013, 9, 9, 8, 30, 0), to: new Date(2013, 9, 9, 12, 0, 0),
                                 progress: {percent: 100, color: '#3C8CF8'}}
                         ]},
-                        {name: 'Create concept', tasks: [
+                        {name: 'Jordan Is Awesome', tasks: [
                             {name: 'Create concept', content: '<i class="fa fa-cog" ng-click="scope.handleTaskIconClick(task.model)"></i> {{task.model.name}}', color: '#F1C232', from: new Date(2013, 9, 10, 8, 0, 0), to: new Date(2013, 9, 16, 18, 0, 0), est: new Date(2013, 9, 8, 8, 0, 0), lct: new Date(2013, 9, 18, 20, 0, 0),
-                                progress: 100}
+                                progress: 50}
                         ]},
-                        {name: 'Finalize concept', tasks: [
-                            {name: 'Finalize concept', color: '#F1C232', from: new Date(2013, 9, 17, 8, 0, 0), to: new Date(2013, 9, 18, 18, 0, 0),
-                                progress: 100}
-                        ]},
-                        {name: 'Development', children: ['Sprint 1', 'Sprint 2', 'Sprint 3', 'Sprint 4'], content: '<i class="fa fa-file-code-o" ng-click="scope.handleRowIconClick(row.model)"></i> {{row.model.name}}'},
-                        {name: 'Sprint 1', tooltips: false, tasks: [
-                            {name: 'Product list view', color: '#F1C232', from: new Date(2013, 9, 21, 8, 0, 0), to: new Date(2013, 9, 25, 15, 0, 0),
-                                progress: 25}
-                        ]},
-                        {name: 'Sprint 2', tasks: [
-                            {name: 'Order basket', color: '#F1C232', from: new Date(2013, 9, 28, 8, 0, 0), to: new Date(2013, 10, 1, 15, 0, 0)}
-                        ]},
-                        {name: 'Sprint 3', tasks: [
-                            {name: 'Checkout', color: '#F1C232', from: new Date(2013, 10, 4, 8, 0, 0), to: new Date(2013, 10, 8, 15, 0, 0)}
-                        ]},
-                        {name: 'Sprint 4', tasks: [
-                            {name: 'Login & Signup & Admin Views', color: '#F1C232', from: new Date(2013, 10, 11, 8, 0, 0), to: new Date(2013, 10, 15, 15, 0, 0)}
-                        ]},
-                        {name: 'Hosting'},
-                        {name: 'Setup', tasks: [
-                            {name: 'HW', color: '#F1C232', from: new Date(2013, 10, 18, 8, 0, 0), to: new Date(2013, 10, 18, 12, 0, 0)}
-                        ]},
-                        {name: 'Config', tasks: [
-                            {name: 'SW / DNS/ Backups', color: '#F1C232', from: new Date(2013, 10, 18, 12, 0, 0), to: new Date(2013, 10, 21, 18, 0, 0)}
-                        ]},
-                        {name: 'Server', parent: 'Hosting', children: ['Setup', 'Config']},
-                        {name: 'Deployment', parent: 'Hosting', tasks: [
-                            {name: 'Depl. & Final testing', color: '#F1C232', from: new Date(2013, 10, 21, 8, 0, 0), to: new Date(2013, 10, 22, 12, 0, 0), 'classes': 'gantt-task-deployment'}
-                        ]},
-                        {name: 'Workshop', tasks: [
-                            {name: 'On-side education', color: '#F1C232', from: new Date(2013, 10, 24, 9, 0, 0), to: new Date(2013, 10, 25, 15, 0, 0)}
-                        ]},
-                        {name: 'Content', tasks: [
-                            {name: 'Supervise content creation', color: '#F1C232', from: new Date(2013, 10, 26, 9, 0, 0), to: new Date(2013, 10, 29, 16, 0, 0)}
-                        ]},
-                        {name: 'Documentation', tasks: [
-                            {name: 'Technical/User documentation', color: '#F1C232', from: new Date(2013, 10, 26, 8, 0, 0), to: new Date(2013, 10, 28, 18, 0, 0)}
-                        ]}
-                    ];
+                        
+			  ];
             },
             getSampleTimespans: function() {
                 return [
